a36a70e6bc55dc28f1ac44f4c7ea90ea589945c7
Ayoyinka Sofuwa <ayoyinkas@gmail.com>
2022-03-09 15:07:25 +0100

    model runs

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,23 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from `dbt-tutorial`.jaffle_shop.customers
+
+)
+
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from `dbt-tutorial`.jaffle_shop.customers
